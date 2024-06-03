5e7ac7ac5352487c24ceb7be69835623f58a6052
Collins <149684399+coll254-strath@users.noreply.github.com>
2024-06-03 13:56:57 +0100

    Add files via upload

---
+++ src/components/pages/Home.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+export const Home = () => {
+  return ( <div>
+  <h1>Welcome 
+    This is the Home page
+  </h1>
+  <br></br>
+  <h3>
+This is a react components project,updates will be pushed to Github
+
+  </h3>
+  </div>
+);
+};
